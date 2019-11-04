c4d8fb654ea99bd90cf0a21a6c4b0735fcaa9614
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2019-11-04 00:57:15 -0300

    Update byte2seg_testbench.vhd

--- byte2seg_testbench.vhd
+++ byte2seg_testbench.vhd
@@ -60,17 +60,3 @@ begin
     wait;
   end process;
 end tb;
-
-  -- stimulus process
-  stim_proc : process
-  begin        
-    for i in 0 to 15 loop
-      byte(7 downto 4) <= conv_std_logic_vector(i,4);
-      byte(3 downto 0) <= conv_std_logic_vector(i,4);
-
-      wait for period;
-    end loop;
-
-    wait;
-  end process;
-end tb;
